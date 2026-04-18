410e9cdf712b5b6be5152b9efb558ed870838c46
makun17060 <makun17060@gmail.com>
2026-04-18 16:14:32 +0900

    修正

--- server-line.js
+++ server-line.js
@@ -3721,7 +3721,9 @@ async function main() {
 
   logInfo("boot completed");
 }
-
+app.get("/__whoami", (req, res) => {
+  res.status(200).send("SERVER-LINE CONFIRMED 2026-04-18");
+});
 main().catch((e) => {
   logErr("boot failed", e?.stack || e);
   process.exit(1);
